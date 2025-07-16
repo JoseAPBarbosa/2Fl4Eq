77465fc021e6aacf10a2e252c0239a4dce08b230
barubozaa <joseapb436@gmail.com>
2025-07-16 11:00:48 -0300

    arquivo de inicializacao criado

--- main.jl
+++ main.jl
@@ -1,64 +1,7 @@
+include("./initialization.jl")
+using LinearAlgebra, Printf, Plots, CSV, DataFrames
+
 include("./position_tuples.jl")
-using LinearAlgebra, JSON, Printf, Plots, CSV, DataFrames
-
-# Entrada de dados
-dados = JSON.parsefile("Entrada.json")
-
-Comp =  dados["Comprimento [m]"]
-D =     dados["Diâmetro [m]"]
-θ =     dados["Inclinação [rad]"]
-tempo = dados["Tempo de simulação [s]"]
-αG_in = dados["Fração de gás na entrada"]
-αG_i =  dados["Fração de gás inicial no duto"]
-ρG_i =  dados["Densidade do gás [kg/m3]"]
-ρL_i =  dados["Densidade do líquido [kg/m3]"]
-uG_in = dados["Velocidade de entrada de gás [m/s]"]
-uL_in = dados["Velocidade de entrada de líquido [m/s]"]
-P_out = dados["Pressão na saída do domínio [Pa]"]
-cG =    dados["Velocidade do som do gás [m/s]"]
-cL =    dados["Velocidade do som do líquido [m/s]"]
-N =     dados["Número de segmentos"]
-CFL =   dados["CFL"]
-Tol_P = dados["Tolerância para a pressão"]
-Tol_u = dados["Tolerância para a velocidades"]
-Tol_α = dados["Tolerância para as frações"]
-
-# Parâmetros
-Area = (pi * D^2) / 4       # Área da seção do duto
-αL_in = 1 - αG_in           # Fração de líquido na entrada
-αL_i = 1 - αG_i             # Fração de líquido inicial
-g = 9.81                    # aceleração gravitacional
-
-Δx = Comp / N               # Passo espacial
-Δt = (CFL * Δx) / uL_in     # Passo temporal
-Nt = Int(tempo ÷ Δt)        # Número de passos temporais
-γ = 1.2                     # coeficiente misterioso
-
-# Variáveis
-## Condições Iniciais
-α_G = αG_i * ones(N)
-α_L = αL_i * ones(N)
-ρ_G = ρG_i * ones(N)
-ρ_L = ρL_i * ones(N)
-u_G = uG_in * ones(N + 1)
-u_L = uL_in * ones(N + 1)
-P_ = P_out * ones(N)
-
-## Supostos
-αØ_G = αG_i * ones(N)
-αØ_L = αL_i * ones(N)
-ρØ_G = ρG_i * ones(N)
-ρØ_L = ρL_i * ones(N)
-uØ_G = uG_in * ones(N + 1)
-uØ_L = uL_in * ones(N + 1)
-PØ_ = P_out * ones(N)
-
-## Erros
-e_uL = 1
-e_uG = 1
-e_αL = 1
-e_αG = 1
-e_P = 1
 
 function momentum_conservation_equation(
     αØ_g::Vector{Float64},
@@ -389,7 +332,7 @@ while (cont < 1) || (e_uL < Tol_u && e_uG < Tol_u && e_αL < Tol_α && e_αG < T
     e_P = norm(P_ - PØ_) / norm(PØ_)
 =#
     cont = cont + 1
-
+    display("ok")
     #display(e_P)
     #display(cont)
 
